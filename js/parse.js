f679db053c44f82a6ee2f8e0574b086625e7f5a8
Patrick Weston <patrickfweston@gmail.com>
2017-12-06 15:07:09 +0800

    Simplify the Javascript (a lot :raised_hands:)

--- js/parse.js
+++ js/parse.js
@@ -5,64 +5,27 @@
  * @returns {Element}
  */
 var str2DOMElement = function(html) {
-    var wrapMap = {
-        option: [ 1, "<select multiple='multiple'>", "</select>" ],
-        legend: [ 1, "<fieldset>", "</fieldset>" ],
-        area: [ 1, "<map>", "</map>" ],
-        param: [ 1, "<object>", "</object>" ],
-        thead: [ 1, "<table>", "</table>" ],
-        tr: [ 2, "<table><tbody>", "</tbody></table>" ],
-        col: [ 2, "<table><tbody></tbody><colgroup>", "</colgroup></table>" ],
-        td: [ 3, "<table><tbody><tr>", "</tr></tbody></table>" ],
-        body: [0, "", ""],
-        _default: [ 1, "<div>", "</div>"  ]
-    };
-    wrapMap.optgroup = wrapMap.option;
-    wrapMap.tbody = wrapMap.tfoot = wrapMap.colgroup = wrapMap.caption = wrapMap.thead;
-    wrapMap.th = wrapMap.td;
-    var match = /<\s*\w.*?>/g.exec(html);
-    var element = document.createElement('div');
-    if(match != null) {
-        var tag = match[0].replace(/</g, '').replace(/>/g, '').split(' ')[0];
-        if(tag.toLowerCase() === 'body') {
-            var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
-            var body = document.createElement("body");
-            // keeping the attributes
-            element.innerHTML = html.replace(/<body/g, '<div').replace(/<\/body>/g, '</div>');
-            var attrs = element.firstChild.attributes;
-            body.innerHTML = html;
-            for(var i=0; i<attrs.length; i++) {
-                body.setAttribute(attrs[i].name, attrs[i].value);
-            }
-            return body;
-        } else {
-            var map = wrapMap[tag] || wrapMap._default, element;
-            html = map[1] + html + map[2];
-            element.innerHTML = html;
-            // Descend through wrappers to the right content
-            var j = map[0]+1;
-            while(j--) {
-                element = element.lastChild;
-            }
-        }
-    } else {
-        element.innerHTML = html;
-        element = element.lastChild;
-    }
-    return element;
+    var frame = document.createElement('iframe');
+    frame.style.display = 'none';
+    document.body.appendChild(frame);
+    frame.contentDocument.open();
+    frame.contentDocument.write(html);
+    frame.contentDocument.close();
+    var el = frame.contentDocument.body.childNodes;
+    document.body.removeChild(frame);
+    return el;
 };
 
 /**
  * Recursively walks through the given DOM and creates a set of selectors.
  * @param node
- * @param ignoreTags
  * @param append
  * @param result
  * @returns {Array.<T>}
  */
-function walk(node, ignoreTags, append, result) {
+function walk(node, append, result) {
     if (typeof append === 'undefined') append = '';
-    if (typeof result === 'undefined') result = new Array();
+    if (typeof result === 'undefined') result = [];
 
     var children = node.childNodes;
     var tempAppend = append;
@@ -74,6 +37,7 @@ function walk(node, ignoreTags, append, result) {
         if ($object.prop("tagName").toLowerCase() == 'svg') {
             continue;
         }
+
 
         var classes = '';
         if ($object.attr('class')) {
@@ -94,7 +58,7 @@ function walk(node, ignoreTags, append, result) {
         }
 
         result.push(tempAppend);
-        walk(children[i], ignoreTags, tempAppend, result);
+        walk(children[i], tempAppend, result);
     }
 
     return result.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
@@ -107,18 +71,7 @@ $(document).ready(function() {
     $('.parse').on('click', function(e) {
         e.preventDefault();
 
-        var dom = str2DOMElement($('.input').val());
-        var queryStrings = walk(dom, ['svg', 'div']);
-
-        var resultString = '';
-        for (var i = 0; i < queryStrings.length; i++) {
-            var region = $('#region').val();
-            if (region) {
-                resultString += "$this->assertRegionElement('" + queryStrings[i] + "', '" + region + "');" + "\n";
-            } else {
-                resultString += queryStrings[i] + "\n";
-            }
-        }
+        var resultString = createBehatTestStrings('.input');
 
         $('#result').show();
         $('#result-tests').html('<pre>'+resultString+'</pre>');
@@ -130,6 +83,36 @@ $(document).ready(function() {
     $('#copy-to-clipboard').on('click', function(e) {
        copyToClipboard('#result-tests');
     });
+
+    /**
+     * Build out our Behat test strings.
+     * 
+     * @param element
+     * @returns {string}
+     */
+    function createBehatTestStrings(element) {
+        // Convert the input into a traversable DOM object.
+        var dom = str2DOMElement($(element).val().trim());
+
+        // Build out our strings for tests
+        var resultString = '';
+        for (var i = 0; i < dom.length; i++) {
+            // Walk through the DOM for this top level item.
+            var queryStrings = walk(dom[i]);
+
+            // Loop over multiple query strings.
+            for (var j = 0; j < queryStrings.length; j++) {
+                var region = $('#region').val();
+                if (region) {
+                    resultString += "$this->assertRegionElement('" + queryStrings[j] + "', '" + region + "');" + "\n";
+                } else {
+                    resultString += queryStrings[i] + "\n";
+                }
+            }
+        }
+
+        return resultString;
+    }
 
     /**
      * Copies an element's value to the clipboard
